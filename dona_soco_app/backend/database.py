71ea32d66500100b1ac135fea195540f136fa203
ivam3 <ivam3.bh@gmail.com>
2026-02-12 03:48:59 -0600

    fixing black screen T3

--- dona_soco_app/backend/database.py
+++ dona_soco_app/backend/database.py
@@ -11,9 +11,9 @@ if DATABASE_URL:
         pool_pre_ping=True
     )
 else:
-    # Local / Railway SQLite → Usar carpeta 'static' que suele ser el volumen
-    os.makedirs("static", exist_ok=True)
-    DATABASE_URL = "sqlite:///./static/backend_dona_soco.db"
+    # Local / Railway SQLite → Usar la subcarpeta 'uploads' que es donde está el volumen
+    os.makedirs("static/uploads", exist_ok=True)
+    DATABASE_URL = "sqlite:///./static/uploads/backend_dona_soco.db"
     engine = create_engine(
         DATABASE_URL,
         connect_args={"check_same_thread": False}

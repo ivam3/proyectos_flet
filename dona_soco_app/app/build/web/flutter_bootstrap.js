a929f51bbed05c19a9c2e4a5f62f5a0929335d10
ivam3 <ivam3.bh@gmail.com>
2026-02-12 18:36:54 -0600

    Fixing upload img T3

--- dona_soco_app/app/build/web/flutter_bootstrap.js
+++ dona_soco_app/app/build/web/flutter_bootstrap.js
@@ -52,7 +52,7 @@ if (flet.noCdn) {
 _flutter.loader.load({
     config: flutterConfig,
     serviceWorkerSettings: {
-        serviceWorkerVersion: "3699412705",
+        serviceWorkerVersion: "246233874",
     },
     onEntrypointLoaded: async function (engineInitializer) {
         const engine = await engineInitializer.initializeEngine(flutterConfig);

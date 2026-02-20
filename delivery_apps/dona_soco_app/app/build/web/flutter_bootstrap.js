58ee20f3a825797d63229571a24f65f786871585
ivam3 <ivam3.bh@gmail.com>
2026-02-20 02:43:58 -0600

    restore main branch 3

--- delivery_apps/dona_soco_app/app/build/web/flutter_bootstrap.js
+++ delivery_apps/dona_soco_app/app/build/web/flutter_bootstrap.js
@@ -52,7 +52,7 @@ if (flet.noCdn) {
 _flutter.loader.load({
     config: flutterConfig,
     serviceWorkerSettings: {
-        serviceWorkerVersion: "2932345734",
+        serviceWorkerVersion: "2851244576",
     },
     onEntrypointLoaded: async function (engineInitializer) {
         const engine = await engineInitializer.initializeEngine(flutterConfig);
